133a6d77ae77fb300e37977c652019bcdbd45db1
eosman04 <112137669+eosman04@users.noreply.github.com>
2024-10-26 17:27:19 -0500

    ESSAM PUSHED
    initial structure for the stack

---
+++ front-end/playlist-generator-ui/src/Pages/Landing/Landing.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import cell from '/perfectform.gif'
+import './Landing.css'
+
+
+export default function Landing() {
+    const [count, setCount] = useState(0)
+    
+    return (
+      
+        <div class>
+            <img src={cell} className="logo" alt="cell aura" />
+            <h1>i actually fuck with bbw's nigga</h1>
+            
+        </div>
+        
+      
+    )
+  }
+  
+  
