a68b623da122b64f58f3406e027cfcebb87ad5ff
Adam Hassan <adamhas132@gmail.com>
2024-11-07 22:23:17 -0600

    Update Navbar.jsx

--- front-end/playlist-generator-ui/src/Components/Navbar.jsx
+++ front-end/playlist-generator-ui/src/Components/Navbar.jsx
@@ -6,21 +6,24 @@ import icon from '/Moodify Icon.png'
 
 const Navbar = () => {
     const navigate = useNavigate();
-  return (    
-    
+
+    function handleClick() {
+        sessionStorage.clear();
+        localStorage.clear();
+        navigate('/login');
+    }
+
+    return (    
         <div id='navbar-wrap'>
             <ul>
                 <li className='logo-and-name' onClick={() => location.reload()}>
-                    <img src={icon} className="logo" alt="Moodify Icon" /> 
-                    <p> Moodify </p> 
+                    <img src={icon} className="logo" alt="Moodify Icon" />
+                    <p> Moodify </p>
                 </li>
-                <li className='login' onClick={() => navigate('/login')}>  Login </li>
+                <li className='login' onClick={handleClick}> Login </li>
             </ul>
         </div>
-
-    
-
-  );
+    );
 };
 
 export default Navbar;
