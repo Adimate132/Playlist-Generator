e7a52dc2cb1cae6d5758214440d2b4fff25666b1
Eduardo <eduardojrz2020@gmail.com>
2024-11-07 22:13:48 -0600

    Front-end changes

--- front-end/playlist-generator-ui/src/Pages/Landing/Landing.jsx
+++ front-end/playlist-generator-ui/src/Pages/Landing/Landing.jsx
@@ -35,46 +35,61 @@ export default function Landing() {
             <h2>Landing</h2> */}
             <div className='page-content'>
                 <div className='menu-wrap'>
-                    <input className='spotify-input' type='text' placeholder='Enter a mood:'/>
-                    {userInfo != null ?
-                        <div className='greet-user'>
-                            <div className='profile-image-wrap'>
-                                <img src={userInfo.user_profile.images[0] ? userInfo.user_profile.images[0].url : logo} alt="your pfp" />
+                    <div className="content-container">
+                        <div className="user-info">
+                            <input className='spotify-input' 
+                                   type='text' 
+                                   placeholder='Enter a mood:'
+                            />
+                            {userInfo != null ? (
+                                <div className='greet-user'>
+                                <div className='profile-image-wrap'>
+                                    <img 
+                                        src={
+                                            userInfo.user_profile.images[0] 
+                                            ? userInfo.user_profile.images[0].url 
+                                            : logo
+                                        } 
+                                        alt="your pfp" 
+                                    />
+                                </div>
+                                <h1> Hello {userInfo.user_profile.display_name}! </h1>
                             </div>
-                            <h1> Hello {userInfo.user_profile.display_name}! </h1>
+                            ) : (// if no user info...
+                                <div className='greet-user'>
+                                    <h1> Loading... </h1>
+                                </div>
+                            )}
                         </div>
-                        : // if no user info...
-                        <div className='greet-user'>
-                            <h1> Loading... </h1>
-                        </div>
-                    }
-
-                    <div className='generated-playlist'>
-                            {complete ?
-                                <h1 className='container-text'> Playlist generated </h1>
-                                :
-                                <h1 className='container-text' > Generating songs based off of your TOP 50 </h1>
-                            }
-                        <div className='song'>
-                            <div> imagine this is a song </div>
-                        </div>
-                        <div className='song'>
-                            <div> imagine this is a song </div>
-                        </div>
-                        <div className='song'>
-                            <div> imagine this is a song </div>
-                        </div>
-                        <div className='song'>
-                            <div> imagine this is a song </div>
+                        {/*Song list (right table)*/}
+                        <div className="song-list">
+                            {complete ? (
+                                <h1 className='container-text'> 
+                                    Playlist generated </h1>
+                             ) : (
+                                <h1 className='container-text' > 
+                                    Generating songs based off of your TOP 50 </h1>
+                            )}
+                            <div className="songs-scroll">
+                                <div className="song">imagine this is a Song</div>
+                                <div className="song">imagine this is a Song</div>
+                                <div className="song">imagine this is a Song</div>
+                                <div className="song">imagine this is a Song</div>
+                                <div className="song">imagine this is a Song</div>
+                                <div className="song">imagine this is a Song</div>
+                                <div className="song">imagine this is a Song</div>
+                                <div className="song">imagine this is a Song</div>
+                                <div className="song">imagine this is a Song</div>
+                                <div className="song">imagine this is a Song</div>
+                                <div className="song">imagine this is a Song</div>
+                                <div className="song">imagine this is a Song</div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-                
         </div>
-        
-      
-    )
+    );
   }
   
   
